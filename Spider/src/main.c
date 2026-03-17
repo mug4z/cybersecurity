5dea7098a9d25fea7a05f1c345052f90f8e4a217
Timothee Pierre-Andre Frily <tfrily@student.42lausanne.ch>
2026-03-17 18:01:26 +0100

    feat(source): add the main and makefile

--- Spider/src/main.c
+++ Spider/src/main.c
@@ -1,3 +1,9 @@
-#include <curl/curl.h>
+//gcc -Wall -Werror -Wextra main.c -L/home/tfrily/.brew/lib -lcurl -o test_curlOD
+#include "/home/tfrily/.brew/include/curl/curl.h"
+#include <stdio.h>
 
+int main(void) { 
+  CURL *curl;
 
+  return 1; 
+}
