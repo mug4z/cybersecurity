10ffd0686b1db339b33f9300febde058ba9153b8
Timothee Pierre-Andre Frily <tfrily@student.42lausanne.ch>
2026-05-06 14:32:47 +0200

    feat(): find images

--- Spider/src/spider.py
+++ Spider/src/spider.py
@@ -1,55 +1,48 @@
 #!/usr/bin/env python3
 
+import re
 import requests
-import os
+# import os
 import bs4
 from bs4 import BeautifulSoup
 
 
-def create_dir(dir_path: str) -> None:
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
+def get_web_page(url: str, headers: dict) -> requests.models.Response:
+    return requests.get(url, headers=headers)
 
 
-def img_finder(data: bs4.BeautifulSoup) -> str:
-    # res = None
-    img = data.find_all('img')
-    
-    print(img)
-    print(type(img))
-    
-    # for link in data.find_all('img'):
-    #     link.get('src')
-    # type(res)
-    return ("lol")
-
-def link_finder(data: bs4.BeautifulSoup):
-    for link in data.find_all('a'):
-        print(link.get('href'))
-
-def BeautifulSoup_test():
-    with open("./data/korben.html", "rb") as file:
-        soup = BeautifulSoup(file,'html.parser')
-    # print(type(soup))
-    # soup = BeautifulSoup("<html>data</html>")
-    # print(soup.find_all('a'))
-    img_finder(soup)
-    # link_finder(soup)
-        
+def img_finder_all(data: bs4.BeautifulSoup) -> str:
+    # lol = data.find_all('img',{'src':re.compile(r'(jpe?g)|(png)|(.gif)|(.bmp)$')})
+    lol = data.find_all('img','.png')
+    print(type(lol))
+    # for link in data.find_all("img"):
+    #     list.append(link.get("src"))
+    # print(list)
+    # NOTE: (jpe?g)|(png)|(.gif)|(.bmp)$ regex to use
+    return "lol"
 
 
-def download_page(url: str, dir: str = "./data/") -> None:
-    create_dir(dir)
-    print(requests.get(url).status_code)
-    with open(dir + "korben.html", "wb") as file:
-        file.write(requests.get(url).content)
-    file.close()
+def link_finder_all(data: bs4.BeautifulSoup):
+    for link in data.find_all("a"):
+        print(link.get("href"))
+
+
+def beautiful_soup_creator(response: str) -> bs4.BeautifulSoup:
+    return BeautifulSoup(response, "html.parser")
 
 
 def main():
-    # print("hello world")
-    download_page("https://korben.info/")
-    BeautifulSoup_test()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:144.0) \
+         Gecko/20100101 Firefox/144.0"
+    }
+    response = get_web_page("https://korben.info/", headers)
+    print(type(headers))
+    if response.status_code != 200:
+        print("NOT GOOD")
+        exit(1)
+    soup = beautiful_soup_creator(response.text)
+    img_finder_all(soup)
 
 
 if __name__ == "__main__":
