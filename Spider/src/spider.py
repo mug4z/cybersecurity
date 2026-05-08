201d2115c80e1f3d55ebbe76e3550b2c3ff99cf0
Timothee Pierre-Andre Frily <tfrily@student.42lausanne.ch>
2026-05-08 14:31:01 +0200

    feat(python): download all image of a page

--- Spider/src/spider.py
+++ Spider/src/spider.py
@@ -2,23 +2,32 @@
 
 import re
 import requests
-# import os
+from urllib.parse import urlsplit
+import os
 import bs4
 from bs4 import BeautifulSoup
-
 
 def get_web_page(url: str, headers: dict) -> requests.models.Response:
     return requests.get(url, headers=headers)
 
+def create_dir(path: str) -> None:
+    if not os.path.isdir(path):
+        os.makedirs(path)
 
-def img_finder_all(data: bs4.BeautifulSoup) -> str:
-    # lol = data.find_all('img',{'src':re.compile(r'(jpe?g)|(png)|(.gif)|(.bmp)$')})
-    data.find_all(src=re.compile(r'(jpe?g)|(png)|(.gif)|(.bmp)$'))
-    # for link in data.find_all("img"):
-    #     list.append(link.get("src"))
-    # print(list)
-    # NOTE: (jpe?g)|(png)|(.gif)|(.bmp)$ regex to use
-    return "lol"
+def download_images(domain_name: str, images: bs4.element.ResultSet, path: str = "./data/",) -> None:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:144.0) \
+         Gecko/20100101 Firefox/144.0"
+    }
+    create_dir(path)
+    for image in images:
+        if not os.path.isfile(image.get('src').split('/')[-1]):
+            with open(path+image.get('src').split('/')[-1],'wb') as file:
+                file.write(get_web_page(domain_name+image.get('src'),headers=headers).content)
+    
+
+def img_finder_all(data: bs4.BeautifulSoup) -> bs4.element.ResultSet:
+    return data.find_all("img", src=re.compile(r"(\.jpe?g)|(\.png)|(\.gif)|(\.bmp)$"))
 
 
 def link_finder_all(data: bs4.BeautifulSoup):
@@ -41,7 +50,8 @@ def main():
         print("NOT GOOD")
         exit(1)
     soup = beautiful_soup_creator(response.text)
-    img_finder_all(soup)
+    images = img_finder_all(soup)
+    download_images("https://korben.info",images)
 
 
 if __name__ == "__main__":
