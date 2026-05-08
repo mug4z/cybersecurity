38d036c55c3f48bd74c5c1c12110168868ccbaee
Timothee Pierre-Andre Frily <tfrily@student.42lausanne.ch>
2026-05-08 17:00:18 +0200

    fix(python): fix an error when the image is external of the website

--- Spider/src/spider.py
+++ Spider/src/spider.py
@@ -6,6 +6,12 @@ from urllib.parse import urlsplit
 import os
 import bs4
 from bs4 import BeautifulSoup
+import argparse
+
+headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:144.0) \
+         Gecko/20100101 Firefox/144.0"
+}
 
 def get_web_page(url: str, headers: dict) -> requests.models.Response:
     return requests.get(url, headers=headers)
@@ -14,16 +20,24 @@ def create_dir(path: str) -> None:
     if not os.path.isdir(path):
         os.makedirs(path)
 
+
+def is_external_img(src: str) -> bool:
+    split_result = urlsplit(src)
+    # print(split_result)
+    if split_result.scheme and split_result.netloc:
+        return True
+    return False
+
+
 def download_images(domain_name: str, images: bs4.element.ResultSet, path: str = "./data/",) -> None:
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:144.0) \
-         Gecko/20100101 Firefox/144.0"
-    }
     create_dir(path)
     for image in images:
+        img_link = image.get('src')
+        if not is_external_img(img_link):
+            img_link = domain_name + img_link
         if not os.path.isfile(image.get('src').split('/')[-1]):
             with open(path+image.get('src').split('/')[-1],'wb') as file:
-                file.write(get_web_page(domain_name+image.get('src'),headers=headers).content)
+                file.write(get_web_page(img_link ,headers=headers).content)
     
 
 def img_finder_all(data: bs4.BeautifulSoup) -> bs4.element.ResultSet:
@@ -40,25 +54,27 @@ def beautiful_soup_creator(response: str) -> bs4.BeautifulSoup:
 
 
 def main():
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:144.0) \
-         Gecko/20100101 Firefox/144.0"
-    }
-    response = get_web_page("https://korben.info/", headers)
-    print(type(headers))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p","--path",help="The path where to download the images")
+    parser.add_argument("url",help="The target website to download from, in the form https://url")
+    args = parser.parse_args()
+    response = get_web_page(args.url, headers)
+    print(response.status_code)
     if response.status_code != 200:
         print("NOT GOOD")
         exit(1)
     soup = beautiful_soup_creator(response.text)
     images = img_finder_all(soup)
-    download_images("https://korben.info",images)
+    # print(images)
+    download_images(args.url,images)
 
 
 if __name__ == "__main__":
     main()
 
 
-# TODO: Function to download all image (jpg/jpeg, .png,.gif, .bmp) from a single page
+# TODO: Function to download all image (jpg/jpeg, .png,.gif, .bmp) from a single page -> DONE
+# TODO: add arguments
 # TODO: Function to follow links
 # TODO: Get all the image from a given url
 # TODO: website to check are korben.info and lwn.net
