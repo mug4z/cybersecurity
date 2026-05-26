17f53a9a2dc9ecf23b167bf0ecc09ba06da5c274
mug4z <mug4z@protonmail.com>
2026-05-26 18:18:23 +0200

    feat(ft_otp): create encrypt and decrypt function

--- ft_otp/ft_otp.py
+++ ft_otp/ft_otp.py
@@ -8,13 +8,66 @@ from hashlib import sha1
 import struct
 import time
 import argparse
+import os
+
+from cryptography.fernet import Fernet
 
 from modules.ft_hotp import gen_HOTP, get_time_counter
+
+def create_key(path_to_key: str):
+    print("Try to create key")
+    try:
+        key = Fernet.generate_key()
+        if os.path.isfile(path_to_key):
+            raise Exception("Key already exist")
+        with open(path_to_key,'wb') as file:
+            file.write(key)
+        print("Key created")
+    except Exception as e:
+        print(f"Faild because of {e}")
+
+def encrypt_file(file_to_encrypt: str, path_to_key: str):
+    try:
+        content_to_encrypt = None
+        f = None
+        with open(path_to_key,'rb') as file:
+            key = file.read()
+            f = Fernet(key)
+            file.close()
+        with open(file_to_encrypt,'rb') as file:
+            content_to_encrypt = file.read()
+            file.close()
+        token = f.encrypt(content_to_encrypt)
+        if os.path.isfile("ft_otp.key"):
+            raise Exception("ft_otp.key already exists")
+        with open('ft_otp.key','wb') as file:
+            file.write(token)
+    except Exception as e:
+        print(f"Faild because of {e}")
+
+def decrypt_file(file_to_decrypt: str, path_to_key: str):
+    try:
+        f = None
+        content_to_encrypt = None
+        with open(path_to_key,'rb') as file:
+            key = file.read()
+            f = Fernet(key)
+            file.close()
+        with open(file_to_decrypt,'rb') as file:
+            content_to_encrypt = file.read()
+            file.close()
+        return f.decrypt(content_to_encrypt)
+
+    except Exception as e:
+        print(f"Faild because of {e}")
+
 
 
 # def gen_TOTP(key_hex: string, time: string):
     
 def main():
+    encrypt_file('key.txt', './.key')
+    print(decrypt_file('ft_otp.key', './.key'))
     parser = argparse.ArgumentParser()
     parser.add_argument("-g",help="The path of the key file, must be at least 64 character in hexadecimal")
     parser.add_argument("-k","--key",action="store_true",help="generate a new temporary password")
