c2c03147efb162abf8606a06f3ba581bf1606fc4
mug4z <mug4z@protonmail.com>
2026-06-03 14:51:29 +0200

    feat(reverse_me): add new information about the binary

--- reverse_me/level3/level3.c
+++ reverse_me/level3/level3.c
@@ -1,5 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void wt(){
+  printf("********");
+}
+void nice(){
+  printf("nice");
+}
+
+void try(){
+  printf("try");
+}
+
+void but(){
+  printf("but");
+}
+
+void this(){
+  printf("this");
+}
+
+void it(){
+  printf("it");
+}
+
+void not(){
+  printf("not");
+}
+
+void that(){
+  printf("that");
+}
+
+void easy(){
+  printf("easy");
+}
+
 int main()
 {
   char *fmt = "%23s";
-  return 0;
+  char tmp[24];
+  char password[24] = "********";
+  printf("Please enter key: ");
+  scanf(fmt, tmp); 
+
+  // ********
+  // 42042042042042042042042
+
+  if (tmp[1] == '2') {
+    if (tmp[0] == '4') {
+
+      fflush(stdin);
+      int idx = 2;
+      char str[9];
+      int pos = 0;
+      memset(str, 0, 9);
+
+      while(strlen(str) < 8 && idx < strlen(tmp) ){
+
+        str[pos++] = atoi((char[4]){tmp[idx], tmp[idx + 1],tmp[idx + 2], '\0'});
+        idx+=3;
+      }
+      printf("%s\n",strcmp(password, str) ? "Nope." : "Good job." );
+
+    }
+    
+  }
 }
